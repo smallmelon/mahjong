a4bbbf852b054872cbfa916f500ba2bfdc60fcce
smallmelon <myfishlgc@163.com>
2015-02-15 20:47:12 +0800

    mod_agent

--- main.lua
+++ main.lua
@@ -5,14 +5,22 @@ local redis = require "lib_redis"
 local mysql = require "lib_mysql"
 local test_logger = require "test_logger"
 
-
+local max_client = 10240
 skynet.start(function ()
     print("Mahjong server start")
     local console = skynet.newservice("console")
     skynet.newservice("debug_console", 8000)
-    -- init redis, mysql pool
-    test_logger:test()
     redis:init()
     mysql:init()
+
+    local watchdog = skynet.newservice("watchdog")
+    skynet.call(watchdog, "lua", "start", {
+        port = 10086,
+        maxclient = max_client,
+        nodelay = true,
+    })
+    print("Watchdog listen on ", 10086)
+
+    skynet.exit()
     
 end)
